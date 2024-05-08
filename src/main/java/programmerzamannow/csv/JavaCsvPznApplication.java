d266f45913514b99a24548bf6d81627e5e33b0a6
Alvenio Farhan Prayogo <alveniofarhan@gmail.com>
2024-05-08 09:53:58 +0700

    Java CSV

---
+++ src/main/java/programmerzamannow/csv/JavaCsvPznApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCsvPznApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCsvPznApplication.class, args);
+	}
+
+}
